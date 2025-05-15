a2d125c0b5e2d101e40ea6cd3a96558b905a5633
Vishal Srinivasan <vishalvishal619@gmail.com>
2025-05-14 21:39:34 -0400

    Add JavaScript file

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('send-button').addEventListener('click', function() {
+    // Placeholder for sending message functionality
+    alert('Send button clicked!  This would send a message in a real application.');
+});
